a0991ac5f06c4d1316b8ee6dc841580a6d491485
Bhismabhavanari89 <86977671+Bhismabhavanari89@users.noreply.github.com>
2023-01-23 20:05:53 +0530

    Add files via upload

---
+++ js/demo4.js
@@ -0,0 +1,19 @@
+function myfun(){
+    var name = fim.nm.value;
+    var des = fim.de.value;
+    if(name==""){
+        alert("Name Required");
+        fim.nm.focus;
+       
+    }
+    else if(des == ""){
+        alert("Designation required");
+    }
+    
+}
+function mynum(){
+    var name = fim.nm.value;
+    if(name==""){
+        alert("Name required");
+    }
+}
